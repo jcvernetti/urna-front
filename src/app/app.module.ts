a4c03a48321c9461388b2187e7cd6791baee1275
Stephanny Sena <stephanny.sena@capgemini.com>
2021-09-20 17:42:41 -0300

    Gerado html admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      LoginComponent
+      LoginComponent,
+      AdminComponent
    ],
   imports: [
     BrowserModule
