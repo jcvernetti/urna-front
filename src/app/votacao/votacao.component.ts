a2b507f6f42898d8115618166f45b7815b3d088c
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-09-22 10:57:07 -0300

    Criação do layout da pagina votação

--- src/app/votacao/votacao.component.ts
+++ src/app/votacao/votacao.component.ts
@@ -1,3 +1,4 @@
+import { Candidato } from './../../../models/candidato.models';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DadosService } from '../service/dados.service';
@@ -9,16 +10,38 @@ import { DadosService } from '../service/dados.service';
 })
 export class VotacaoComponent implements OnInit {
 
+  nomeCandidato: string = "";
+  numeroCandidato: string = "";
+  private _candidatos: Array<Candidato> = [];
+
   constructor(private service: DadosService, private router: Router) { }
 
-
-  public isLogado(){
-
-    return localStorage.getItem("isLogado") == "true";
-
+  ngOnInit() {
+    this.service.getAllCandidatos().subscribe((auxCandidatos: Array<Candidato>) => {
+      this.candidatos = auxCandidatos;
+    });
   }
 
-  ngOnInit() {
+  public get candidatos(): Array<Candidato>  {
+		return this._candidatos;
+	}
+
+	public set candidatos(value: Array<Candidato> ) {
+		this._candidatos = value;
+	}
+
+  public getNomeCandidato(){
+    let auxCandidato: any = this.candidatos.find(candidato => candidato._numero == Number(this.numeroCandidato));
+
+    if(this.numeroCandidato == "") {
+      this.nomeCandidato = "";
+    } else {
+      this.nomeCandidato = auxCandidato == undefined ? "Candidao não encontrado": auxCandidato.nome;
+    }
+  }
+
+  public isLogado(){
+    return localStorage.getItem("isLogado") == "true";
   }
 
 }
