61ddec88bde4277a50279a73605e6fe280b86437
Welisson Hudson Fernandes Lima <welisson.lima@capgemini.com>
2021-09-22 15:39:36 -0300

    Enviando os dados do voto para serem salvos

--- src/app/votacao/votacao.component.ts
+++ src/app/votacao/votacao.component.ts
@@ -12,6 +12,7 @@ export class VotacaoComponent implements OnInit {
 
   nomeCandidato: string = "";
   numeroCandidato: string = "";
+  private elementoDesabilitado: Boolean = false;
   private _candidatos: Array<Candidato> = [];
 
   constructor(private service: DadosService, private router: Router) { }
@@ -36,7 +37,7 @@ export class VotacaoComponent implements OnInit {
     if(this.numeroCandidato == "") {
       this.nomeCandidato = "";
     } else {
-      this.nomeCandidato = auxCandidato == undefined ? "Candidao não encontrado": auxCandidato.nome;
+      this.nomeCandidato = auxCandidato == undefined ? "Candidao não encontrado": auxCandidato._nome;
     }
   }
 
@@ -44,4 +45,24 @@ export class VotacaoComponent implements OnInit {
     return localStorage.getItem("isLogado") == "true";
   }
 
+  public votar(){
+    let nome: string = this.nomeCandidato == undefined ? "Nulo": this.nomeCandidato;
+    let numero: number =  isNaN(Number(this.numeroCandidato)) ? -1: Number(this.numeroCandidato);
+    let dataVoto: Date = new Date();
+
+    let voto: object = {nomeCandidato: nome, numeroCandidato: numero, dataVoto};
+    
+    this.enviarVoto(voto);
+  }
+
+  private enviarVoto(voto: Object): void{
+    this.service.enviarVoto(voto).subscribe((resposta) => {
+      console.log(resposta);
+    });
+  }
+
+  public limparTela(){
+    this.nomeCandidato = "";
+    this.numeroCandidato = "";
+  }
 }
